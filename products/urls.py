8a97b00341b847a6a117cc6b651d4d4047010fa0
hinex-vaghadiya <148621260+hinex-vaghadiya@users.noreply.github.com>
2026-01-06 11:53:17 +0530

    Update urls.py added active endpoint

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 from django.urls import path,include
-from products.views import ProductsViewset
+from products.views import ProductsViewset,ActivenowView
 
 router=routers.DefaultRouter()
 router.register(r'products',ProductsViewset)
+router.register(r'active',ActivenowView)
 
 urlpatterns=[
     path('',include(router.urls))
