fc39fb55b31c7e56b50500d78676c4e266f5ea21
lumuslens <lumuslenstemp@gmail.com>
2026-01-08 17:18:01 +0530

    configure seriallzers little schema change added batch code and is_active to batch model

--- batch/serializers.py
+++ batch/serializers.py
@@ -4,4 +4,4 @@ from batch.models import Batch
 class BatchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Batch
-        fields = ['batch_id', 'variant', 'qty', 'mfg_date', 'exp_date']
+        fields = ['batch_id', 'variant', 'qty', 'mfg_date', 'exp_date','is_active','batch_code']
